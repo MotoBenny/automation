26b48696d8533ab54edbbd0922a49be08bc81423
Benjamin Carter <benj.james.carter@gmail.com>
2022-05-12 11:22:44 -0700

    initial commit of validate phone nums

---
+++ automation/automation.py
@@ -0,0 +1,13 @@
+import re
+
+
+
+phone_reg = '^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$'
+#
+# email_reg
+#
+#
+# match_obj = re.match(phone_reg, POTENTIAL CONTACTS DOC)
+
+def validate_phone_nums():
+    pass
